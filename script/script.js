1f047b367b5da80bcf2ad3452a3953a11f4824b2
minzyee <vvzmsqufvv@gmail.com>
2022-11-08 11:59:42 +0900

    코드 수정

--- script/script.js
+++ script/script.js
@@ -3,10 +3,19 @@
 const startBtn = document.querySelector('.startBtn');
 const pauseBtn = document.querySelector('.pauseBtn');
 const resetBtn = document.querySelector('.resetBtn');
-const timeInput = document.querySelectorAll('.timer-func li');
+const timeInput = document.querySelector('.timer-func li input');
 
 
 
 timeInput.addEventListener('click', () => {
-	
+	startBtn.classList.add('on');
+	resetBtn.classList.add('on');
+});
+
+startBtn.addEventListener('click', () => {
+	if(타이머셋팅 시) {
+		// 타이머 실행
+	} else {
+		alert('시간을 입력해주세요.');
+	}
 });
