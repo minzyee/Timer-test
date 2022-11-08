a56c005f7ead4557333806aafadafe45a0a5be28
minzyee <vvzmsqufvv@gmail.com>
2022-11-08 11:48:34 +0900

    첫 커밋

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+
+const startBtn = document.querySelector('.startBtn');
+const pauseBtn = document.querySelector('.pauseBtn');
+const resetBtn = document.querySelector('.resetBtn');
+const timeInput = document.querySelectorAll('.timer-func li');
+
+
+
+timeInput.addEventListener('click', () => {
+	
+});
